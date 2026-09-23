dc397552bdc4a2322eb5d05b91ebc2c2dfcec95a
Kalesha Vali <kaleshavalid2005@gmail.com>
2026-09-23 12:23:02 +0530

    Refine TitleRow with SVG ShareIcon and HeartIcon

--- src/components/TitleRow.jsx
+++ src/components/TitleRow.jsx
@@ -1,12 +1,19 @@
 import LISTING from "../data/listingData";
+import { ShareIcon, HeartIcon } from "./Icons";
 
 export default function TitleRow() {
   return (
     <div className="title-row">
       <h1>{LISTING.title}</h1>
       <div className="title-actions">
-        <button className="link-btn">↗ Share</button>
-        <button className="link-btn">♡ Save</button>
+        <button className="link-btn" aria-label="Share this listing">
+          <ShareIcon size={16} />
+          <span>Share</span>
+        </button>
+        <button className="link-btn" aria-label="Save this listing">
+          <HeartIcon size={16} />
+          <span>Save</span>
+        </button>
       </div>
     </div>
   );
